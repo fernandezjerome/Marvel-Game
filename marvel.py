c0ef37967a4675271760da840b41c28cc5873e10
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2022-04-01 19:07:02 -0400

    declaring the game funtions

---
+++ marvel.py
@@ -0,0 +1,18 @@
+
+
+
+# this function for new marvel game
+def new_marvelgame():
+    pass
+
+# this function  will determine if the answers were correct
+def check_marvelanswer():
+    pass
+
+# this function will show the scores
+def show_score():
+    pass
+
+# this fuction will ask the player if you will play again
+def play_again():
+    pass
