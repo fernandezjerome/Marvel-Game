6fcaaff8e515657593bd8d5a22454534e7de69fe
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2022-04-01 19:29:59 -0400

    declaring question with the corresponding answer

--- marvel.py
+++ marvel.py
@@ -16,3 +16,19 @@ def show_score():
 # this fuction will ask the player if you will play again
 def play_again():
     pass
+
+
+
+
+
+questiion = {
+"who is a Marvel character who's alter ego in a nuclear scientist? ": "D",
+"This character played by Tom Holland? ": "C",
+"who's the richest character in Marvel'? ": "B",
+"He's a disfigured mercenary with the superhuman ability of regeneration and physical prowess? ": "A",
+}
+
+option = [["A. Wolverine", "B. Spider-Man","C. Thor", "D. Hulk"],
+          ["A. Captain America", "B. Daredevil","C. Spider-Man.", "D. Punisher"],
+          ["A. Nightcrawler", "B. Iron Man","C. Ice Man", "Colossus"],          
+          ["A. Deadpool", "B. Spider-Man","C. Silver Surfer", "Gambit"]]
